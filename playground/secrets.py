4af9ecef41b733eb22d799b2fa06407480c4898f
Harry Smith <38967972+OxyOCE@users.noreply.github.com>
2024-03-04 15:32:10 +1300

    Add some models and admin

--- playground/secrets.py
+++ playground/secrets.py
@@ -3,7 +3,7 @@ from os import getenv
 from dotenv import dotenv_values
 
 def read_secrets():
-    possible_secrets = ['DJANGO_SECRET_KEY']
+    possible_secrets = ['DJANGO_SECRET_KEY', 'DJANGO_TIMEZONE']
     secrets = dotenv_values(".env")
     for key in possible_secrets:
         if key not in secrets:
--- playground/settings.py
+++ playground/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -109,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = SECRETS['DJANGO_TIMEZONE'] if SECRETS['DJANGO_TIMEZONE'] else 'UTC'
 
 USE_I18N = True
 
